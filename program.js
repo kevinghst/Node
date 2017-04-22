4969c12f9d325b55e851789b87c89ee420c0dab4
nick.char.nick@gmail.com <wancongzhang@gmail.com>
2017-04-22 16:28:08 -0400

    finish challenge

--- program.js
+++ program.js
@@ -1,18 +1,17 @@
-const ogFolder = './files/original';
-const fs = require('fs');
+let ogFolder = './files/original';
+let fs = require('fs');
 let renameModule = require('./lib/rename');
+let appendModule = require('./lib/append');
 
 fs.readdir(ogFolder, (err, files) => {
   let errors = 0;
   let count = 0;
 
   files.forEach(file => {
-    //file equivalent to file name
 
     let nameArray = file.split(".");
     let fileName = nameArray[0];
     let fileType = nameArray[1];
-
 
     fs.readFile(`${ogFolder}/${file}`, function(err, data){
       count += 1;
@@ -25,25 +24,8 @@ fs.readdir(ogFolder, (err, files) => {
       }
 
       try{
-        if(fileType === 'json'){
-          let json;
-          if(data.length===0){
-            json = {"boo": "test"};
-          } else {
-            json = JSON.parse(data);
-            json.boo = 'test';
-          }
-
-          fs.writeFile(`${ogFolder}/${file}`, JSON.stringify(json));
-        } else {
-          let appended = data + "\nnew stuff";
-          fs.writeFile(`${ogFolder}/${file}`, appended);
-        }
-        let oldPath = ogFolder + '/' + file;
-        let currentDate = getCurrentDate();
-        let newPath = './files/moved/' + fileName + '_EDITED_' + currentDate + fileType;
-
-        renameModule.rename(oldPath, newPath);
+        appendModule.append(fileType, data, ogFolder, file);
+        renameModule.rename(ogFolder, file, fileName, fileType);
       }
       catch(err){
         errors += 1;
@@ -59,25 +41,3 @@ fs.readdir(ogFolder, (err, files) => {
     });
   });
 });
-
-function getCurrentDate(){
-  let date = new Date();
-  let year = date.getFullYear().toString();
-  let month = date.getMonth().toString();
-  if(month.length === 1){
-    month = '0' + month;
-  }
-  let day = date.getDate().toString();
-  if(day.length === 1){
-    day = '0' + day;
-  }
-  let hour = date.getHours().toString();
-  if(hour.length === 1){
-    hour = '0' + hour;
-  }
-  let minute = date.getMinutes().toString();
-  if(minute.length === 1){
-    minute = '0' + minute;
-  }
-  return `${year}-${month}-${day}_${hour}-${minute}.`;
-}
